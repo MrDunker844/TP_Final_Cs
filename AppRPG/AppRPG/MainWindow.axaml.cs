afd2d7a5c17f6802e00143a0ad434a63808ae9bb
MrDunker844 <134291152+MrDunker844@users.noreply.github.com>
2025-12-12 16:23:54 +0100

    premier commit

---
+++ AppRPG/AppRPG/MainWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia.Controls;
+namespace AppRPG;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        PageAjoutPerso PremierePage = new PageAjoutPerso();
+        
+        
+    }
+}
